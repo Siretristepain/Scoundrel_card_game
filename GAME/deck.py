63741eedb56f42e8789678bcc0276868767978ce
Siretristepain <raphael.ollier7@gmail.com>
2025-11-01 13:13:21 +0100

    [IMP] Add methods to Deck() class
    New methods are :
    - get_card(index) : return the Card at the 'index' position in the Deck.
    - have_in(target) : check if the 'target' Card is in the Deck.
    - add_top_card(card) : add the 'card' Card to the top of the Deck.

--- GAME/deck.py
+++ GAME/deck.py
@@ -70,14 +70,99 @@ class Deck():
 
         return card
     
+    def get_card(self, index: int):
+        """
+        Method used to get the Card at the 'index' position in the Deck.
+
+        Args:
+            - index (int) : the position of the Card in the Deck we want to get.
+
+        Returns:
+            - card (Card()) : the Card at the 'index' position in the Deck. Could be None (ex: if we're out of range).
+        """
+
+        if self.is_empty():
+            return None
+
+        if index >= self.get_size():
+            return None
+
+        card = self.head
+
+        for i in range(index):
+            card = card.next
+
+        return card
+
+    def have_in(self, target_card: Card=None):
+        """
+        Method used to check if the 'target_card' is in the Deck.
+
+        Args:
+            - target_card (Card()) : the Card object that we want to check the presence in Deck.
+
+        Returns:
+            - (bool) : True if 'target_card' in Deck, False otherwise.
+        """
+
+        # We take the top Card of the Deck.
+        card = self.head
+
+        # We check if that first Card is the target one.
+        if card == target_card:
+            return True
+
+        # We go through all the Card in the Deck and we always check if it's the target one.
+        for i in range(self.get_size()):
+            card = card.next
+
+            if card == target_card:
+                return True
+
+        return False
+
+    def add_top_card(self, card_to_add: Card=None):
+        """
+        Method used to add a Card at the top of the Deck.
+
+        Args:
+            - card_to_add (Card()) : the Card object to add to the top of the Deck.
+
+        Returns:
+            - (bool) : True if success, False if the Card to add is None or have no value.
+        """
+
+        # We don't add the Card if it's None or have a null value.
+        if card_to_add == None or card_to_add.is_null():
+            return False
+
+        # We take the actual top Card (before adding) in order to make the 'card_to_add' pointer go to this one.
+        actual_top_card = self.head
+
+        # We put the new top Card
+        self.head = card_to_add
+
+        # We make the new top Card pointer to the older one.
+        card_to_add.next = actual_top_card
+
+        return True
 
 if __name__ == '__main__':
     C1 = Card('2', 'Spades')
     C2 = Card('K', 'Diamonds')
+    C3 = Card('Q', 'Diamonds')
 
     C1.next = C2
 
     D = Deck()
+    D.head = C1
     print(D.get_size())
-    print(D.get_first_card())
-    print(D.get_last_card())
+    # print(D.get_first_card())
+    # print(D.get_last_card())
+    # print(D.get_card(2))
+    print(D.have_in(C2))
+    print(D.have_in(C3))
+    print(D.add_top_card(C3))
+    print(D.have_in(C3))
+    print(D.get_size())
+
